69ebf113b891f39a22245299e6bf8fb9ba80353b
GeorgeH23 <>
2023-06-03 23:51:29 +0000

    Add event for start button #3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-console.log("Football Quiz");
+const title = document.getElementById('game-section-h1');
+const startButton = document.getElementById('start-button');
+const usernameModal = document.getElementById('username-modal');
+const usernameInput = document.getElementById('username-input');
+const usernameSubmit = document.getElementById('username-submit');
+
+let username = '';
+
+startButton.addEventListener('click', showUsernameModal);
+
+// Show username modal when start button is clicked
+function showUsernameModal() {
+    startButton.classList.add('hide');
+    title.classList.add('hide');
+    usernameModal.style.display = 'block';
+}
